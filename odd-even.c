efc71c92aaf118fe5b7244da701166d46a0975de
Snehal Sanjay Mankar <68723397+mankarsnehal@users.noreply.github.com>
2023-04-27 21:29:21 +0530

    Create odd-even.c

---
+++ odd-even.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Enter a number: ");
+    int n = scanf();
+    if (n%2 == 0) {
+        printf("Even Number.");
+    }
+    else {
+        printf("Odd Number");
+    }
+        
+    return 0;
+}
